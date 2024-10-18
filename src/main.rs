05ced8f5eacac5b008841d92a311ed3a9a442807
senyoudev <younesmeskafe2018@gmail.com>
2024-10-19 00:41:18 +0100

    refactoring: refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
+use std::process::Output;
+
 use anyhow::{Context, Result};
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-fn main() -> Result<()> {
-    let args: Vec<_> = std::env::args().collect();
-    let command = &args[3];
-    let command_args = &args[4..];
-    let output = std::process::Command::new(command)
-        .args(command_args)
+fn execute_command(command: &str, args: &[String]) -> Result<std::process::Output> {
+    std::process::Command::new(command)
+        .args(args)
         .output()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
-                command, command_args
+                command, args
             )
-        })?;
-    
+        })
+}
+
+fn print_output(output: &Output) -> Result<()> {
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
         print!("{}", std_out);
@@ -23,6 +23,24 @@ fn main() -> Result<()> {
     } else {
         std::process::exit(1);
     }
+
+    Ok(())
+}
+
+// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+fn main() -> Result<()> {
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() < 4 {
+        eprintln!("Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...");
+        std::process::exit(1);
+    }
+
+    let command = &args[3];
+    let command_args = &args[4..];
+
+    let output = execute_command(command, command_args)?;
+    let _ = print_output(&output);
 
     Ok(())
 }
